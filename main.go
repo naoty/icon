f9d26a4bc8b24323893536d4c7c0055bdc51117d
Naoto Kaneko <naoty.k@gmail.com>
2017-10-13 19:38:52 +0900

    Generate blank image

--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/png"
 	"os"
 )
 
 func main() {
-	fmt.Println("TODO: generate an icon")
+	file, err := os.Create("icon.png")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	image := image.NewRGBA(image.Rect(0, 0, 16, 16))
+	err = png.Encode(file, image)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	os.Exit(0)
 }
